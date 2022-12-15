759b1407b651b347624c3905f7c6805e6d5df3c6
freshcodeAlice <freshjs.ofte@gmail.com>
2022-12-15 12:00:16 +0200

    getCount task resolve

--- controllers/Task.controller.js
+++ controllers/Task.controller.js
@@ -7,7 +7,7 @@ module.exports.createTask = async(req, res, next) => {
         const {body, params: {userId}} = req;
         const user = await User.findByPk(userId);
         const result = await user.createTask(body);
-        res.status(201).send(result);
+        return res.status(201).send(result);
     } catch(error) {
         next(error)
     }
@@ -18,13 +18,20 @@ module.exports.getAllUserTasks = async(req, res, next) => {
        const {params: {userId}} = req;
        const user = await User.findByPk(userId);
        const tasks = await user.getTasks();
-       res.status(200).send(tasks);
+       return res.status(200).send(tasks);
     } catch(error) {
         next(error)
     }
 }
 
 
-module.exports.getCountOfTasks = async() => {
-    
+module.exports.getCountOfTasks = async(req, res, next) => {
+    try {
+        const {params: {userId}} = req;
+        const user = await User.findByPk(userId);
+        const tasks = await user.countTasks();
+        return res.status(200).send(`${tasks}`);
+     } catch(error) {
+         next(error)
+     }
 }
