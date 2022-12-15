56e1dfef2d9876c2267cf2315a72ba9b69609db7
freshcodeAlice <freshjs.ofte@gmail.com>
2022-12-15 10:13:54 +0200

    Tasks task resolve

---
+++ migrations/20221215081011-create-task.js
@@ -0,0 +1,39 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('tasks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      body: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      isDone: {
+        field: 'is_done',
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+      },
+      deadline: {
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        field: 'created_at',
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        field: 'updated_at',
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('tasks');
+  }
+};
